0247874806099b0cb478b45314dd1b829ef81406
Christopher-Leccia <christo.lca@live.fr>
2018-04-17 15:29:33 +0200

    1er partie insertion donnée
    Manque le rajout de photo

--- manga/db/seeds.rb
+++ manga/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+mangas.destroy_all
+
+mangas.create!(name: "Tokyo Ghoul:Re", author: "Sui Ishida", nb_tome: 16)
+mangas.create!(name: "Bleach", author: "Tite Kubo", nb_tome: 74)
+mangas.create!(name: "One piece", author: "Eiichirō Oda", nb_tome: 88)
+mangas.create!(name: "Nanatsu no Taizai", author: "Nakaba Suzuki", nb_tome: 30)
+mangas.create!(name: "Fullmetal Alchemist", author: "Hiromu Arakawa", nb_tome: 27)
